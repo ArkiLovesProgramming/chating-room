a290800928a288a7c48816427f4b8fefd23605f9
arkiloveprogramming <betaweiwang@gmail.com>
2023-07-03 11:59:56 -0230

    Initialize code

---
+++ src/main/java/com/concurrent/chatingroom/ChatingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.concurrent.chatingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatingRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatingRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concurrent/chatingroom/ChatingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurrent.chatingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatingRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
